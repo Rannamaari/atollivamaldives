53f2261e5499a2d7782aff321dc7e577a5d11bb7
Munad <munad@local>
2026-08-24 18:04:10 +0500

    Clean generated files from repository

--- storage/framework/views/8355eca347641e0df19d6e2665fa5048.php
+++
@@ -1,4 +0,0 @@
-<a class="logo <?php echo e($dark ?? false ? 'dark' : ''); ?>" href="<?php echo e(route('home')); ?>" aria-label="ATOLLIVA MALDIVES home">
-    <img src="<?php echo e(asset('logo/Atolliva Maldives Logo Transparent.png')); ?>" alt="ATOLLIVA MALDIVES" class="logo-image">
-</a>
-<?php /**PATH /Users/munad/Documents/Micro Travel/Micro-Travel-Laravel/resources/views/partials/logo.blade.php ENDPATH**/ ?>
